72f4acf76d1c363cb053f6eafa0a2d1abba696f8
thebigsmileXD <thebigsmilexd@gmail.com>
2017-01-27 07:52:40 +0100

    Fix #35 ItemFrame drops

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -32,6 +32,7 @@ use pocketmine\entity\Human;
 use pocketmine\entity\Item as DroppedItem;
 use pocketmine\entity\Living;
 use pocketmine\entity\Projectile;
+use pocketmine\event\block\ItemFrameDropItemEvent;
 use pocketmine\event\entity\EntityDamageByBlockEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
@@ -3081,23 +3082,25 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 				}
 				break;
 			case ProtocolInfo::ITEM_FRAME_DROP_ITEM_PACKET:
-				if($this->spawned === false or $this->blocked === true or !$this->isAlive()){
+				if ($this->spawned === false or $this->blocked === true or !$this->isAlive()) {
 					break;
 				}
-
-				if(($tile = $this->level->getTile($this->temporalVector->setComponents($packet->x, $packet->y, $packet->z))) instanceof ItemFrame){
-					if(!$tile->getItem()->equals($packet->item) and !$this->isCreative(true)){
+				$tile = $this->level->getTile($this->temporalVector->setComponents($packet->x, $packet->y, $packet->z));
+				if ($tile instanceof ItemFrame) {
+					if (!$tile->getItem()->equals($packet->item)) {
 						$tile->spawnTo($this);
 						break;
 					}
-
-					if(lcg_value() <= $tile->getItemDropChance() and $packet->item->getId() !== Item::AIR){
-						$this->level->dropItem($tile->getBlock(), $packet->item); //Use the packet item to handle creative drops correctly
+					$this->getServer()->getPluginManager()->callEvent($ev = new ItemFrameDropItemEvent($this->getLevel()->getBlock($tile), $this, $tile->getItem(), $tile->getItemDropChance()));
+					if (!$ev->isCancelled()) {
+						if (lcg_value() <= $ev->getItemDropChance() && $packet->item->getId() !== Item::AIR) {
+							if($this->getGamemode() !== self::CREATIVE && $this->getGamemode() !== self::SPECTATOR)
+								$this->level->dropItem($tile, $ev->getDropItem());
+						}
+						$tile->setItem();
+						//$tile->setItemRotation(0); //This actually doesn't happen.
 					}
-					$tile->setItem(null);
-					$tile->setItemRotation(0);
 				}
-
 				break;
 			default:
 				break;
